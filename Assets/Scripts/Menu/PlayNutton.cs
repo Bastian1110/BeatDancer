9cff3ad26375d0415ab1ec68e9b6fd6dd59955af
Harumi Cristal <A01751568@itesm.mx>
2022-03-23 20:24:48 -0600

    Button Menu Test
    A little test to try harumi collab

---
+++ Assets/Scripts/Menu/PlayNutton.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayNutton : MonoBehaviour
+{
+    public void TestFunction()
+    {
+        print("Boton Play presionado!!!");
+    }
+}
