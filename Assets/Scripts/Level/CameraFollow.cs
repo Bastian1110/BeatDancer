95942f6b257042da9c0b64ec113d127a980b7e99
Harumi Cristal <A01751568@itesm.mx>
2022-04-04 20:29:48 -0500

    cámara y fondo
    Actualización del código para que la cámara y fondo siga al personaje cuando va caminando.

--- Assets/Scripts/Level/CameraFollow.cs
+++ Assets/Scripts/Level/CameraFollow.cs
@@ -5,11 +5,12 @@ using UnityEngine;
 // Script to make the camera follow the player
 public class CameraFollow : MonoBehaviour
 {
+    public int distancia = 0;
     public Transform followTransform;
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        this.transform.position = new Vector3(followTransform.position.x, followTransform.position.y,this.transform.position.z);
+        this.transform.position = new Vector3(followTransform.position.x + distancia, this.transform.position.y,this.transform.position.z);
     }
 }
