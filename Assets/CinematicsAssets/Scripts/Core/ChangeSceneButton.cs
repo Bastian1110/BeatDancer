a5b9a5bb005b2b0a6c65a49a1bc69cd01e3e75f8
Germán Guzmán <83972918+GermanGuzmanLopez@users.noreply.github.com>
2022-05-03 13:55:30 -0500

    Quit Button

--- Assets/CinematicsAssets/Scripts/Core/ChangeSceneButton.cs
+++ Assets/CinematicsAssets/Scripts/Core/ChangeSceneButton.cs
@@ -9,4 +9,10 @@ public class ChangeSceneButton : MonoBehaviour
     {
         SceneManager.LoadScene(sceneName);
     }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+        Debug.Log("Quit");
+    }
 }
