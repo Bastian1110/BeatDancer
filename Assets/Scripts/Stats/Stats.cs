680b07fd716fef62e6d90eecca61d8a750ef81f6
A01746163 <69471795+A01746163@users.noreply.github.com>
2022-05-03 12:50:02 -0500

    Added Stats Function

---
+++ Assets/Scripts/Stats/Stats.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stats : MonoBehaviour
+{
+    public void OpenURL()
+    {
+        Application.OpenURL("https://public.tableau.com/shared/KSN8QHQYC?:display_count=n&:origin=viz_share_link");
+    }
+}
